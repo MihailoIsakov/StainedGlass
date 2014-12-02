b6ef5f7e8c46f0a5099405198e02966c8d2a512c
Zieghailo <isakov.m@gmail.com>
2014-12-02 18:07:41 +0100

    cleaned up a bit

--- trimath.py
+++ trimath.py
@@ -6,7 +6,32 @@ import matplotlib.tri as tri
 from plotter import plot_triangles
 
 
-def triangle_sum(img, tr):
+def _y_intersects(y, tr):
+    """
+    Returns the points where the triangle lines intersect the horizontal y line
+    :param y: height of the horizontal line
+    :param tr: triangle 2x3 numpy array
+    :return: the three points where the line intersects the lines. Some may be NaN in case of a horizontal line.
+    """
+    diffAB = tr[:, 1] - tr[:, 0]
+    diffAC = tr[:, 2] - tr[:, 0]
+    diffBC = tr[:, 2] - tr[:, 1]
+
+    kAB = diffAB[1] / diffAB[0]
+    kAC = diffAC[1] / diffAC[0]
+    kBC = diffBC[1] / diffBC[0]
+
+    dxab = (y - tr[1, 0]) / kAB
+    xab = tr[0, 0] + dxab
+    dxac = (y - tr[1, 0]) / kAC
+    xac = tr[0, 0] + dxac
+    dxbc = (y - tr[1, 1]) / kBC
+    xbc = tr[0, 1] + dxbc
+
+    return np.array([xab, xac, xbc])
+
+
+def triangle_sum(img, tr, get_error = False):
     north = np.floor(np.amax(tr[1,:])).astype(int)
     south = np.ceil (np.amin(tr[1,:])).astype(int)
     east  = np.floor(np.amax(tr[0,:])).astype(int)
@@ -14,25 +39,11 @@ def triangle_sum(img, tr):
 
     num_of_pixels = 0
     sum = np.array([0, 0, 0])
+    error = np.array([0, 0, 0])
+    bounds = np.zeros([north - south + 1, 2])
 
     for y in range(south, north + 1):
-        diffAB = tr[:, 1] - tr[:, 0]
-        diffAC = tr[:, 2] - tr[:, 0]
-        diffBC = tr[:, 2] - tr[:, 1]
-
-        kAB = diffAB[1] / diffAB[0]
-        kAC = diffAC[1] / diffAC[0]
-        kBC = diffBC[1] / diffBC[0]
-
-        dxab = (y - tr[1, 0]) / kAB
-        xab = tr[0, 0] + dxab
-        dxac = (y - tr[1, 0]) / kAC
-        xac = tr[0, 0] + dxac
-        dxbc = (y - tr[1, 1]) / kBC
-        xbc = tr[0, 1] + dxbc
-
-        sol = np.array([xab, xac, xbc])
-
+        sol = _y_intersects(y, tr)
         sol = sol[west <= sol]
         sol = sol[sol <= east]
 
@@ -41,13 +52,20 @@ def triangle_sum(img, tr):
 
         left  = np.round(np.amin(sol)).astype(int)
         right = np.round(np.amax(sol)).astype(int)
+        bounds[y - south] = [left, right]
 
         sum += np.sum(img[left:right + 1, y], axis = 0)
         num_of_pixels += right - left + 1
 
     num_of_pixels = 1 if num_of_pixels == 0 else num_of_pixels
     sum[0], sum[2] = sum[2], sum[0] # cv2 issues
-    return tuple(sum / num_of_pixels / 255.0)
+    color = sum / num_of_pixels
+
+    if get_error:
+        for y in range(south, north + 1): # get the error of the triangle
+            error += np.sum(np.linalg.norm(img[bounds[y - south, 0] : bounds[y - south, 1]] - color))
+
+    return tuple(color / 255.0)
 
 
 def random_delaunay(img, n):
@@ -72,17 +90,21 @@ def delaunay_color(img, dln):
         sys.stdout.write('\r[' + '-' * (50 * ind / l) + ' ' * (50 - 50 * ind / l) + ']')
         triang = np.array([dln.x[dln.triangles[ind]], dln.y[dln.triangles[ind]]])
         colors[ind] = triangle_sum(img, triang)
+
+    print ' '
     return colors
 
 
 def main():
+    np.seterr(all = 'ignore')
+
     print("Trimath main")
     import cv2
     img = cv2.imread('images/tara.jpg')
     # img = np.flipud(img)
 
     print "Running Delaunay triangulation"
-    dln = random_delaunay(img, 5000)
+    dln = random_delaunay(img, 500)
 
     print "Caluclating triangle colors"
     colors = delaunay_color(img, dln)
