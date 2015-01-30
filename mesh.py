846da9fc157494a8be2b0817a2bcf8a5c2d54dd6
Zieghailo <isakov.m@gmail.com>
2015-01-30 22:50:15 +0100

    Merge and stash fuckup

--- mesh.py
+++ mesh.py
@@ -12,16 +12,7 @@ from multiprocessing import Pool
 from random import sample
 from heapq import nlargest, nsmallest
 
-# region needed so that @profile doesn't cause an error
-import __builtin__
 
-try:
-    __builtin__.profile
-except AttributeError:
-    # No line profiler, provide a pass-through version
-    def profile(func): return func
-    __builtin__.profile = profile
-# endregion
 
 
 class Mesh(object):
