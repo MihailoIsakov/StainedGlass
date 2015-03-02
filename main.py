b33d708a4ec060d1254831c7e7f7c8832e70f241
Zieghailo <isakov.m@gmail.com>
2015-03-02 15:18:09 +0100

    Working command line arguments. Single settings file supported.

--- main.py
+++ main.py
@@ -13,29 +13,27 @@ import cv2
 import numpy as np
 from time import time
 
-from settings.renoir_settings import *
-
 
 @profile
-def main():
+def main(C):
     global mesh
 
-    img = cv2.imread(IMAGE_URI)
+    img = cv2.imread(C.IMAGE_URI)
     cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
 #    img = np.flipud(img)
 
     trimath.set_image(img)
 
-    mesh = Mesh(img, STARTING_POINTS, parallel=PARALLEL)
+    mesh = Mesh(img, C.STARTING_POINTS, parallel=C.PARALLEL)
     mesh.triangulate(True)
 
-    col = FlatMeshCollection(mesh._triangulation, alpha=TRIANGLE_ALPHA)
+    col = FlatMeshCollection(mesh._triangulation, alpha=C.TRIANGLE_ALPHA)
     plotter.start()
-    plotter.plot_original(img, 1 - TRIANGLE_ALPHA)
+    plotter.plot_original(img, 1 - C.TRIANGLE_ALPHA)
     plotter.plot_mesh_collection(col)
     past = time()
 
-    pixtemp = TEMPERATURE  # pixels radius
+    pixtemp = C.TEMPERATURE  # pixels radius
 
     for cnt in range(10 ** 6):
 
@@ -43,55 +41,58 @@ def main():
         # In the beginning when pixtemp is almost TEMPERATURE,
         # the chance to purge is high. As the temperature gets lower,
         # the chance to purge approaches zero.
-        purge = np.random.rand() * PURGE_MULTIPLIER < pixtemp / TEMPERATURE
+        purge = np.random.rand() * C.PURGE_MULTIPLIER < pixtemp / C.TEMPERATURE
 
         mesh.evolve(pixtemp,
                     purge=purge,
-                    parallel=PARALLEL)
-        pixtemp *= TEMP_MULTIPLIER
+                    parallel=C.PARALLEL)
+        pixtemp *= C.TEMP_MULTIPLIER
 
         # region print time
-        if PRINT_CONSOLE:
+        if C.PRINT_CONSOLE:
             print("Temperature: "+str(pixtemp))
             now = time()
             print("Time elapsed: ", now - past)
             past = now
         #endregion
 
-        if (cnt % PRINT_ERROR_COUNTER == 0):
+        if (cnt % C.PRINT_ERROR_COUNTER == 0):
             plotter.plot_global_errors(mesh._error)
 
-        if (cnt % PRINT_COUNTER == (PRINT_COUNTER - 1)):
-            print TRIANGLE_ALPHA
-            col = FlatMeshCollection(mesh._triangulation, alpha=TRIANGLE_ALPHA)
+        if (cnt % C.PRINT_COUNTER == (C.PRINT_COUNTER - 1)):
+            print C.TRIANGLE_ALPHA
+            col = FlatMeshCollection(mesh._triangulation, alpha=C.TRIANGLE_ALPHA)
             #plotter.plot_points(mesh)
             #plotter.plot_arrow(mesh)
-            plotter.plot_original(img, 1 - TRIANGLE_ALPHA)
+            plotter.plot_original(img, 1 - C.TRIANGLE_ALPHA)
             plotter.plot_mesh_collection(col)
             # plotter.plot_error_hist(mesh.point_errors, mesh.triangle_errors)
 
     plotter.keep_plot_open()
 
 
-def parse_arguments():
+def parse_arguments(default_settings):
     import argparse
     parser = argparse.ArgumentParser(prog='Stained Glass',
                                      description='Create a low poly version '
                                                  'of a given image.')
-    parser.add_argument('filename')
+    parser.add_argument('IMAGE_URI')
     parser.add_argument('-t', '--temperature', type=float, dest='TEMPERATURE')
     parser.add_argument('-p', '--points',      type=int,   dest='STARTING_POINTS')
     parser.add_argument('-m', '--multiplier',  type=float, dest='TEMP_MULTIPLIER')
     parser.add_argument('-c', '--console',     type=bool,  dest='PRINT_CONSOLE')
     parser.add_argument('-d', '--draw',        type=int,   dest='PRINT_COUNTER')
+    parser.add_argument('-a', '--alpha',       type=float, dest='TRIANGLE_ALPHA')
 
-    args = parser.parse_args()
-    print args
-    return args
+    return parser.parse_args(namespace=default_settings)
 
 
 if __name__ == "__main__":
+    from settings.default_settings import C
 
-    globals().update(vars(parse_arguments()))
+    settings = parse_arguments(C)
 
-    main()
+    print(settings)
+    print C
+
+    main(C)
---
+++ settings/default_settings.py
@@ -0,0 +1,16 @@
+__author__ = 'zieghailo'
+
+from argparse import Namespace
+
+C = Namespace()
+C.IMAGE_URI              = 'images/renoir.jpg'
+C.STARTING_POINTS        = 500
+C.TEMPERATURE            = 5
+C.TEMP_MULTIPLIER        = 0.9997
+C.PURGE_MULTIPLIER       = 1  # 1 = 100%
+C.PARALLEL               = True
+C.PRINT                  = True
+C.PRINT_COUNTER          = 10
+C.PRINT_CONSOLE          = True
+C.PRINT_ERROR_COUNTER    = 10
+C.TRIANGLE_ALPHA         = 0.6
